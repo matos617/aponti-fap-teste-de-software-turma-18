d10c4f9c1b2aafa291a5b47de949a6de5d8e93a2
Carla Beatriz Matos <mattosdelima617@gmail.com>
2026-09-03 12:16:39 -0300

    Add files via upload

---
+++ módulo-02/src/hello.ts
@@ -0,0 +1,12 @@
+// 1 variaveis e tipos primitivos
+const saudacao: string = "Olá, mundo!";
+let ano: number = 2026;
+
+const focado: boolean = true;
+
+console.log(saudacao);
+
+let totalAlunos: number = 35;
+totalAlunos = 33;
+
+console.log(`Temos hoje ${totalAlunos} no curso do FAP de Teste de Software do ano ${ano}. 👍`)
